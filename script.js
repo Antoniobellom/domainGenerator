4ab461ee275ed66253fbe36803a142b183895b88
Antoniobellom <61624925+Antoniobellom@users.noreply.github.com>
2021-11-09 10:04:28 -0300

    first

---
+++ script.js
@@ -0,0 +1,18 @@
+var pronoun = ['the', 'our'];
+var adj = ['great', 'big'];
+var noun = ['jogger', 'racoon'];
+let domain = ['.com','.cl','.net']
+
+
+    for(let a of pronoun){
+        for(let b of adj){
+            for(let c of noun){
+                for(let d of domain){
+                   
+                    console.log(a + b +c +d );
+                }
+            }
+        }
+    }
+  
+
